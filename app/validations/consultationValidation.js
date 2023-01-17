684ab833faea34ff65f5741332fefa67b8720609
fajarbagass <fajarbagassetiawan17@gmail.com>
2023-01-17 11:02:02 +0700

    update consultation validation

--- app/validations/consultationValidation.js
+++ app/validations/consultationValidation.js
@@ -6,5 +6,7 @@ module.exports = {
     body("client_id", "Client is not valid").isNumeric(),
     body("indication_id", "Indication is required").exists(),
     body("indication_id", "Indication is not valid").isNumeric(),
+    body("cf_user", "CF user is required").exists(),
+    body("cf_user", "CF user is not valid").isFloat(),
   ],
 };
